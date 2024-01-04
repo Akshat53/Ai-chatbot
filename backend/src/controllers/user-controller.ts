bca3b0873d9b9700fa42d8891ed57e5983c7d305
Akshat singh <singh.akshat.akshat26@gmail.com>
2024-01-04 16:11:44 +0530

    endpoints updated

--- backend/src/controllers/user-controller.ts
+++ backend/src/controllers/user-controller.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import User from "../models/User.js";
-import { hash } from "bcrypt";
+import { compare, hash } from "bcrypt";
 
 export const getAllUser = async (
   req: Request,
@@ -23,17 +23,42 @@ export const userSignUp = async (
 ) => {
   try {
     const { name, email, password } = req.body;
+
     const hashedPassword = await hash(password, 10);
+    const existingUser = await User.findOne({ email });
+    if (existingUser)
+      return res.status(401).json("user already exist with this email id ");
     const user = new User({ name, email, password: hashedPassword });
     await user.save();
-    return res
-      .status(200)
-      .json({
-        message: "user signed up successfully",
-        id: user._id.toString(),
-      });
+    return res.status(201).json({
+      message: "user signed up successfully",
+      id: user._id.toString(),
+    });
   } catch (error) {
     console.log(error);
+    return res.status(200).json({ message: "ERROR", cause: error.message });
+  }
+};
+export const userLogin = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(401).json("user not exist");
+    }
+    const checkPassword = await compare(password, user.password);
+    if (!checkPassword) {
+      return res.status(403).json("Entered password is incorrect");
+    }
+    return res
+      .status(200)
+      .json({ message: "ok", id: user._id.toString(), name: user.name });
+  } catch (error) {
     return res.status(200).json({ message: "ERROR", cause: error.message });
   }
 };
--- backend/src/routes/user-routes.ts
+++ backend/src/routes/user-routes.ts
@@ -1,10 +1,11 @@
 import {Router} from "express";
-import { getAllUser, userSignUp } from "../controllers/user-controller.js";
+import { getAllUser, userSignUp,userLogin } from "../controllers/user-controller.js";
 
 
 
 const userRouter = Router();
 userRouter.get("/",getAllUser);
 userRouter.post("/signup",userSignUp)
+userRouter.post("/login",userLogin)
 
 export default userRouter;
